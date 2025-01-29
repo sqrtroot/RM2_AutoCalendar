114740a4e6d2d7db99e89040b5858a2bf35fc476
Robert <robert@sqrtroot.com>
2025-01-30 00:34:18 +0100

    Added appointment blocks

--- src/components/DayPage.tsx
+++ src/components/DayPage.tsx
@@ -1,7 +1,7 @@
 import { Page, Text, View, StyleSheet } from "@react-pdf/renderer";
 import { getDayOfYear, intlFormat } from "date-fns";
 import { Appointment } from "../data/Appointment";
-
+import { randomUUIDv7 } from "bun";
 
 const styles = StyleSheet.create({
   page: {
@@ -32,14 +32,23 @@ const styles = StyleSheet.create({
     border: "1pt solid black",
     borderLeft: "none",
   },
+  appointmentBlock: {
+    position: "absolute",
+    left: "9%",
+    width: "40%",
+    backgroundColor: "#ccc",
+    padding: 5,
+    border: "1pt solid black",
+    borderRadius: 5,
+    textOverflow:'ellipsis',
+  },
+  appointmentText: {
+    fontSize: 5,
+  },
 });
 
 // Helper function to generate hours
-const generateHours = (
-  start: number,
-  end: number,
-  step: number = 1
-) => {
+const generateHours = (start: number, end: number) => {
   const hours = [];
   const height = `${100 / (end - start)}%`;
   for (let i = start; i <= end; i++) {
@@ -62,17 +71,51 @@ const generateHours = (
   return hours;
 };
 
+
 export const DayPage = (props: { day: Date; appointments: Appointment[] }) => {
   const hours = generateHours(8, 21);
+  const appointmentBlocks = props.appointments.map((appointment) => {
+    const startHour = appointment.start.getHours();
+    const endHour = appointment.end.getHours();
+    const hourSpan = endHour - startHour;
+    return (
+      <View
+        key={appointment.summary+randomUUIDv7()}
+        style={[
+          styles.appointmentBlock,
+          {
+            top: `${(startHour - 8) * (100 / 14)}%`,
+            height: `${hourSpan * (100 / 14)}%`,
+          },
+        ]}
+      >
+        <Text style={styles.appointmentText}>
+          {appointment.summary}{" "}
+          {appointment.location ? "@ " + appointment.location : ""}
+        </Text>
+        <Text style={styles.appointmentText}>
+          {intlFormat(appointment.start, {
+            hour: "numeric",
+            minute: "numeric",
+          })}
+          -{intlFormat(appointment.end, { hour: "numeric", minute: "numeric" })}
+        </Text>
+      </View>
+    );
+  });
   return (
     <Page size="A4" style={styles.page} id={"" + getDayOfYear(props.day)}>
       <Text style={styles.title}>
-        {intlFormat(props.day, { day: "numeric", month: "long"})}
+        {intlFormat(props.day, { day: "numeric", month: "long" })}
       </Text>
       <View style={{ flexDirection: "row" }}>
         <View style={styles.timetableContainer}>{hours}</View>
-        <View style={styles.todoContainer}></View>
+        <View style={styles.todoContainer}>
+          <Text style={{ textAlign: "center" }}>TODO</Text>
+        </View>
+        {appointmentBlocks}
       </View>
     </Page>
   );
 };
+1
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import { Document } from "@react-pdf/renderer";
 import { DayPage } from "./components/DayPage";
 import { Appointment } from "./data/Appointment";
-export const Calendar = (props:{year: number}) => {
+export const Calendar = (props: { year: number }) => {
   return (
     <Document>
       <DayPage
         day={new Date(props.year, 0, 1)}
         appointments={[
-          new Appointment("Meeting", "Meeting room", new Date(props.year, 0, 1, 9, 0), new Date(props.year, 0, 1, 10, 0)),
+          new Appointment(
+            "Meeting",
+            "Meeting room",
+            new Date(props.year, 0, 1, 10, 0),
+            new Date(props.year, 0, 1, 12, 0)
+          ),
+          new Appointment(
+            "Dinner",
+            "Restaurant",
+            new Date(props.year, 0, 1, 18, 0),
+            new Date(props.year, 0, 1, 20, 0)
+          ),
         ]}
       />
     </Document>
